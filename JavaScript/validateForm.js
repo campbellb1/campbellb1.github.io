679aaef0f4846a145e4a9afbf944489ceb72c102
Unknown <campbellb1@etsu.edu>
2018-07-01 22:01:13 -0400

    Had to remove password from validation js

--- JavaScript/validateForm.js
+++ JavaScript/validateForm.js
@@ -10,22 +10,17 @@ function validateForm()
 {
 if ($("#addContact")[0].checkValidity() === true)
 {
-    var passwordsMatch = true;
     var foundEmptyField = false;
     var inputForms = document.getElementById("addContact").querySelectorAll("input");
 
 
     Array.from(inputForms).forEach(function (field)
     {
-
-
         const strValue = "" + field.value;
         if (strValue === null || strValue === '')
         {
             foundEmptyField = true;
-            return;
         }
-
     });
 
     if (foundEmptyField)
@@ -33,13 +28,8 @@ if ($("#addContact")[0].checkValidity() === true)
         alert("Please Enter Values for All of the Fields");
     }
 
-    if (document.querySelector('#password').value !== document.querySelector('#confirmPassword').value)
-    {
-        passwordsMatch = false;
-        alert("The Passwords do not match!");
-    }
 
-    if (passwordsMatch && !foundEmptyField)
+    if (!foundEmptyField)
     {
         return true;
     }
